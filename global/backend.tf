e5c00771ef64f80286dad4e5fdfb990609876ec3
Andrew Oppon <opponandrew@outlook.com>
2026-05-15 23:18:21 +0100

    feat(ci): add OIDC authentication to Terraform backend configuration

--- global/backend.tf
+++ global/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "nxr-authflow-tfstate-rg"
+    storage_account_name = "nxrauthflowtfstate"
+    container_name       = "tfstate"
+    key                  = "authflow/global/global.tfstate"
+    use_oidc             = true
+  }
 }

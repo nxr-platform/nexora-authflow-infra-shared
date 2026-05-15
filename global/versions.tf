4d260998b26de7e1980b3254cd5e29ced2060be9
Andrew Oppon <opponandrew@outlook.com>
2026-05-15 23:37:02 +0100

    feat(ci): update Terraform version and backend config for OIDC auth

--- global/versions.tf
+++ global/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.15.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.110"
+      version = "~> 4.0"
     }
   }
 }

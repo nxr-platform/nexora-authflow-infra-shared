e465c2dbc8faf28d120062ada3a3249e0a6ed313
Andrew Oppon <opponandrew@outlook.com>
2026-06-06 16:58:45 +0100

    feat(global): add ACR to global layer
    Adds Container Registry to global shared infrastructure.
    ACR is a platform-level resource shared across all environments.
    Admin disabled and public network access disabled by default.
    
    Refs: PLAT-032

--- global/outputs.tf
+++ global/outputs.tf
@@ -10,3 +10,18 @@ output "resource_group_location" {
   description = "The location of the global resource group"
   value       = module.resource_group.location
 }
+
+output "acr_id" {
+  description = "The ID of the Container Registry"
+  value       = module.acr.id
+}
+
+output "acr_name" {
+  description = "The name of the Container Registry"
+  value       = module.acr.name
+}
+
+output "acr_login_server" {
+  description = "The login server URL of the Container Registry"
+  value       = module.acr.login_server
+}
--- global/variables.tf
+++ global/variables.tf
@@ -14,3 +14,30 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "acr_sku" {
+  description = "SKU for the Container Registry (Basic, Standard, Premium)"
+  type        = string
+  default     = "Basic"
+}
+
+variable "acr_admin_enabled" {
+  description = "Whether to enable admin access for the Container Registry"
+  type        = bool
+  default     = false
+}
+
+variable "acr_public_network_access_enabled" {
+  description = "Whether to enable public network access to the Container Registry"
+  type        = bool
+  default     = false
+}
+
+variable "acr_georeplications" {
+  description = "List of regions to geo-replicate the registry. Premium SKU only."
+  type = list(object({
+    location                = string
+    zone_redundancy_enabled = optional(bool, false)
+  }))
+  default = []
+}

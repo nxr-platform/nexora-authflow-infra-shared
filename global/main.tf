419b104bf3742d4a436a88f96f11860844240efa
Andrew Oppon <opponandrew@outlook.com>
2026-06-06 17:03:26 +0100

    feat(global): add ACR to global layer
    Adds Container Registry to global shared infrastructure.
    ACR is a platform-level resource shared across all environments.
    Admin disabled and public network access disabled by default.
    
    Refs: PLAT-032

--- global/main.tf
+++ global/main.tf
@@ -32,12 +32,12 @@ module "resource_group" {
 module "acr" {
   source = "git::https://github.com/nxr-platform/nexora-authflow-terraform-modules.git//modules/acr?ref=v0.9.0"
 
-  name                          = module.naming.names.acr
-  resource_group_name           = module.resource_group.name
-  location                      = var.location
-  admin_enabled                 = var.acr_admin_enabled
-  sku                           = var.acr_sku
-  public_network_access_enabled = var.acr_public_network_access_enabled
-  georeplication                = var.acr_geo_replication
-  tags                          = local.common_tags
+  name                           = module.naming.names.acr
+  resource_group_name            = module.resource_group.name
+  location                       = var.location
+  admin_enabled                  = var.acr_admin_enabled
+  sku                            = var.acr_sku
+  public_network_access_enabled  = var.acr_public_network_access_enabled
+  georeplications                = var.acr_geo_replication
+  tags                           = local.common_tags
 }
